13a4b5cf36ae364d9fcf304738590825893f013d
Mehmet (P.R.M.) Misset <mehmet.misset@gmail.com>
2025-06-12 22:16:29 +0200

    initial setup
    initial setup

---
+++ 4-processing-python/main.py
@@ -0,0 +1,30 @@
+# This is the main script to run the data pipeline for the PSA and DTA datasets.
+# It is the main entry point for the data pipeline and is used to run the data pipeline for all datasets.
+# It is also used to run the data pipeline for a specific dataset.
+import sys
+sys.path.append('modules')
+
+# Import Custom Modules
+import modules.run as run
+
+# Set Debugging to "1" => true
+is_debugging = "1"
+
+# Assumtions: stuff a overarching procedure shoudl extract, but for our example we will hardcode it
+id_model          = "5f5640501f5751571f564c505f435854" # was id_model was updated by the initialization
+id_dataset        = "000c0a0c060f0f01000e0c01090c1602" # was id_dataset was updated by the initialization
+ds_external_reference_id = "ds_external_reference_id" # was ds_external_reference_id was updated by the initialization
+nm_target_dataset = 'psa_yahoo_stocks'
+nm_target_table   = 'rf'    
+
+# Extraction of metadata for the desired model + dataset
+run.data_pipeline(id_model, nm_target_dataset, nm_target_table, is_debugging)
+
+
+
+# rebuild html documentation for main pagepip
+# run.export_documentation('-1', is_debugging)
+
+# Process all datasets
+
+print("all done")
